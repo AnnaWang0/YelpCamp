d8a4c525192e525a9d388d21d6bf1e843ff8fb31
Anna Wang <wyywsu@outlook.com>
2022-07-20 15:17:34 -0400

    catchAsync Added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { mongoose } from "mongoose";
 import methodOverride from "method-override";
 import ejsMate from "ejs-mate";
+import catchAsync from "./utils/catchAsync.js";
 import path from "path";
 import Campground from "./models/campground.js";
 
@@ -39,7 +40,7 @@ app.get('/campgrounds', async (req, res) => {
 
 app.get('/campgrounds/new', async (req, res) => {
     res.render('campgrounds/new');
-})
+});
 
 app.post('/campgrounds', async (req, res, next) => {
     try {
@@ -50,33 +51,33 @@ app.post('/campgrounds', async (req, res, next) => {
         next(e);
     }
 
-})
+});
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
-});
+}));
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
-});
+}));
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
-});
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-})
+}));
 
 app.use((err, req, res, next) => {
     res.send('Oh boy, something went wrong!');
-})
+});
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
